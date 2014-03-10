e646429ce4d1835e76f36593dd2f2514c39f72fc
Elizabeth <Elizabeth@10.189.13.88>
2014-03-10 13:09:17 -0700

    Red board line follower. Works in main with no task. PWM is working FYI PWM cannot go over 100

---
+++ Line_Follower/_build/L2_Drivers/src/lpc_pwm.d
@@ -0,0 +1,22 @@
+L2_Drivers/src/lpc_pwm.d: ../L2_Drivers/src/lpc_pwm.cpp \
+ C:\Users\Elizabeth\Documents\SJSU_Dev\projects\Line_Follower\L2_Drivers/lpc_pwm.hpp \
+ C:\Users\Elizabeth\Documents\SJSU_Dev\projects\Line_Follower\L0_LowLevel/LPC17xx.h \
+ C:\Users\Elizabeth\Documents\SJSU_Dev\projects\Line_Follower\L0_LowLevel/source/bit_manip.h \
+ C:\Users\Elizabeth\Documents\SJSU_Dev\projects\Line_Follower\L0_LowLevel/source/lpc_peripherals.h \
+ C:\Users\Elizabeth\Documents\SJSU_Dev\projects\Line_Follower\L0_LowLevel/core_cm3.h \
+ C:\Users\Elizabeth\Documents\SJSU_Dev\projects\Line_Follower\L0_LowLevel/LPC17xx.h \
+ C:\Users\Elizabeth\Documents\SJSU_Dev\projects\Line_Follower/sys_config.h
+
+C:\Users\Elizabeth\Documents\SJSU_Dev\projects\Line_Follower\L2_Drivers/lpc_pwm.hpp:
+
+C:\Users\Elizabeth\Documents\SJSU_Dev\projects\Line_Follower\L0_LowLevel/LPC17xx.h:
+
+C:\Users\Elizabeth\Documents\SJSU_Dev\projects\Line_Follower\L0_LowLevel/source/bit_manip.h:
+
+C:\Users\Elizabeth\Documents\SJSU_Dev\projects\Line_Follower\L0_LowLevel/source/lpc_peripherals.h:
+
+C:\Users\Elizabeth\Documents\SJSU_Dev\projects\Line_Follower\L0_LowLevel/core_cm3.h:
+
+C:\Users\Elizabeth\Documents\SJSU_Dev\projects\Line_Follower\L0_LowLevel/LPC17xx.h:
+
+C:\Users\Elizabeth\Documents\SJSU_Dev\projects\Line_Follower/sys_config.h:
