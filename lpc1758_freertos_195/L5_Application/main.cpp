a6acb4f26e412282454bfeaff252130c184f26e6
airybeartoe <evelasquez123@yahoo.com>
2014-03-11 18:02:22 -0700

    Changed main.cpp to have two tasks running

--- lpc1758_freertos_195/L5_Application/main.cpp
+++ lpc1758_freertos_195/L5_Application/main.cpp
@@ -25,100 +25,48 @@
  * @note  printf of %f may be turned off to save memory, this can be configured at sys_config.h
  */
 #include "tasks.hpp"
+#include "utilities.h"
 #include "examples/examples.hpp"
+#include "stdio.h"
+#include "io.hpp"
+#include "fat/disk/spi_flash.h"
+#include "eint.h"
+#include "uart0_min.h"
 
+//xQueueHandle qh = 0; //global queue handle
 
+void task_rx(void*p)
+{
+    while(1)
+    {
+        //trial 1.a message
+        //uart0_puts("Inside of rx.");
 
-/**
- * The main() creates tasks or "threads".  See the documentation of scheduler_task class at cpp_task.hpp
- * for details.  There is a very simple example towards the beginning of this class's declaration.
- *
- * @warning SPI #1 bus usage notes (interfaced to SD & Flash):
- *      - You can read/write files from multiple tasks because it automatically goes through SPI semaphore.
- *      - If you are going to use the SPI Bus in a FreeRTOS task, you need to use the API at L4_IO/fat/spi_sem.h
- *
- * @warning SPI #0 usage notes (Nordic wireless)
- *      - This bus is more tricky to use because if FreeRTOS is not running, the RIT interrupt may use the bus.
- *      - If FreeRTOS is running, then wireless task may use it.
- *        In either case, you should avoid using this bus or interfacing to external components because
- *        there is no semaphore configured for this bus and it should be used exclusively by nordic wireless.
- */
+        //trial 2.a message
+        uart0_puts("Airybeartoe is my name, im task 1.");
+
+        vTaskDelay(1000);
+    }
+}
+
+void task_tx(void*p)
+{
+    while(1)
+    {
+        //Trial 1.b message
+        //uart0_puts("Inside of the tx task.");
+
+        //Trial 2.b message
+        uart0_puts("Task two, I am. How are you today?");
+        vTaskDelay(1500);
+    }
+}
+
 int main(void)
 {
-    /**
-     * A few basic tasks for this bare-bone system :
-     *      1.  Terminal task provides gateway to interact with the board through UART terminal.
-     *      2.  Remote task allows you to use remote control to interact with the board.
-     *      3.  Wireless task responsible to receive, retry, and handle mesh network.
-     *
-     * Disable remote task if you are not using it.  Also, it needs ENABLE_TELEMETRY
-     * such that it can save remote control codes to non-volatile memory.  IR remote
-     * control codes can be learned by typing "learn" command.
-     */
-    scheduler_add_task(new terminalTask(PRIORITY_HIGH));
-    scheduler_add_task(new remoteTask  (PRIORITY_LOW));
-
-    /* Consumes very little CPU, but need highest priority to handle mesh network ACKs */
-    scheduler_add_task(new wirelessTask(PRIORITY_CRITICAL));
-
-    /* Your tasks should probably used PRIORITY_MEDIUM or PRIORITY_LOW because you
-     * want the terminal task to always be responsive so you can poke around in
-     * case something goes wrong.
-     */
-
-    /**
-     * This is a the board demonstration task that can be used to test the board.
-     * This also shows you how to send a wireless packets to other boards.
-     */
-    #if 0
-        scheduler_add_task(new example_io_demo());
-    #endif
-
-    /**
-     * Change "#if 0" to "#if 1" to enable examples.
-     * Try these examples one at a time.
-     */
-    #if 0
-        scheduler_add_task(new example_task());
-        scheduler_add_task(new example_alarm());
-        scheduler_add_task(new example_logger_qset());
-        scheduler_add_task(new example_nv_vars());
-    #endif
-
-    /**
-	 * Try the rx / tx tasks together to see how they queue data to each other.
-	 */
-    #if 0
-        scheduler_add_task(new queue_tx());
-        scheduler_add_task(new queue_rx());
-    #endif
-
-    /**
-     * Another example of shared handles and producer/consumer using a queue.
-     * In this example, producer will produce as fast as the consumer can consume.
-     */
-    #if 0
-        scheduler_add_task(new producer());
-        scheduler_add_task(new consumer());
-    #endif
-
-    /**
-     * If you have RN-XV on your board, you can connect to Wifi using this task.
-     * This does two things for us:
-     *   1.  The task allows us to perform HTTP web requests (@see wifiTask)
-     *   2.  Terminal task can accept commands from TCP/IP through Wifly module.
-     *
-     * To add terminal command channel, add this at terminal.cpp :: taskEntry() function:
-     * @code
-     *     // Assuming Wifly is on Uart3
-     *     addCommandChannel(Uart3::getInstance(), false);
-     * @endcode
-     */
-    #if 0
-        Uart3::getInstance().init(WIFI_BAUD_RATE, WIFI_RXQ_SIZE, WIFI_TXQ_SIZE);
-        scheduler_add_task(new wifiTask(Uart3::getInstance(), PRIORITY_LOW));
-    #endif
-
-    scheduler_start(true); ///< This shouldn't return
-    return -1;
+   //qh= xQueueCreate(1, sizeof(int));
+   xTaskCreate(task_tx, (const char*)"t1", STACK_BYTES(1024), 0, 1, 0);
+   xTaskCreate(task_rx, (const char*)"t2", STACK_BYTES(1024), 0, 2, 0);
+   vTaskStartScheduler();
+   return -1;
 }
