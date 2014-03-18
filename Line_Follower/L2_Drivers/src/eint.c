3893d803954db5b107e704ddbd472e1244fa29ba
Elizabeth <Elizabeth@10.189.12.40>
2014-03-18 16:02:14 -0700

    updated interrupt to respond to speed trigger

--- Line_Follower/L2_Drivers/src/eint.c
+++ Line_Follower/L2_Drivers/src/eint.c
@@ -1,123 +1,80 @@
-/*
- *     SocialLedge.com - Copyright (C) 2013
- *
- *     This file is part of free software framework for embedded processors.
- *     You can use it and/or distribute it as long as this copyright header
- *     remains unmodified.  The code is free for personal use and requires
- *     permission to use in a commercial product.
- *
- *      THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
- *      OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
- *      MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
- *      I SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR
- *      CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
- *
- *     You can reach the author of this software at :
- *          p r e e t . w i k i @ g m a i l . c o m
- */
+////////////////////////////////////////////// eint.c/////////////////
+
 #include <stdlib.h>
 #include "eint.h"
-
-
-
-/// Linked list structure of EINTs (External interrupts)
-typedef struct eint3_entry {
-    uint64_t pin_mask;          ///< P0 and P2 concatenated pin-mask
-    void_func_t callback;       ///< Callback when interrupt occurs
-    struct eint3_entry* next;   ///< The pointer to the next entry
-} eint3_entry_t;
-
-static eint3_entry_t *m_eint3_list_head = NULL; ///< Instance of list of EINTS
-static const uint8_t m_port2_offset = 32;       ///< Offset of Port2
-
-
-
-/// Actual ISR Handler (mapped to startup file's interrupt vector function name)
-#ifdef __cplusplus
-extern "C" {
-#endif
-void EINT3_IRQHandler(void)
-{
-    uint64_t status = 0;
-    eint3_entry_t *e = m_eint3_list_head;
-
-    /* Get status of PORT0 and PORT2, and store to 64-bit variable */
-    status = (LPC_GPIOINT->IO2IntStatR | LPC_GPIOINT->IO2IntStatF);
-    status <<= m_port2_offset;
-    status |= (LPC_GPIOINT->IO0IntStatR | LPC_GPIOINT->IO0IntStatF);
-
-    /* Loop through our list to find which pin triggered this interrupt */
-    while (e && status) {
-        /* If we find the pin, make the callback and clear the interrupt source*/
-        if (e->pin_mask & status) {
-            status &= ~(e->pin_mask);
-            (e->callback)();
-
-            /* Clear the interrupt based on either Port0 or Port2 */
-            if (e->pin_mask & UINT32_MAX) {
-                LPC_GPIOINT->IO0IntClr = e->pin_mask;
-            }
-            else {
-                LPC_GPIOINT->IO2IntClr = (e->pin_mask >> m_port2_offset);
-            }
-        }
-        e = e->next;
-    };
-
-    /* In case interrupt handler not attached correctly, clear all interrupts here */
-    if (status) {
-        LPC_GPIOINT->IO0IntClr = 0xFFFFFFFF;
-        LPC_GPIOINT->IO2IntClr = 0xFFFFFFFF;
-    }
-}
-#ifdef __cplusplus
-}
-#endif
-
-
+#include <printf_lib.h>
+#include "utilities.h"
 
 /**
- * @param pin_number 0-31 for PORT0, and 32-63 for PORT2
- * @param type       The type of the interrupt (rising or falling)
- * @param func       The callback function.
- */
-static void eint3_enable(uint8_t pin_num, eint_intr_t type, void_func_t func)
+* Enables the callback when the interrupt occurs. The entry added last is checked
+* first if multiple interrupts occur at the same time. Each call will allocate
+* 16 bytes for the interrupt service entry.
+* @note EINT3 shares interrupt with Port0 and Port2
+* @param pin_num The pin number from 0-31.
+* @param type The type of interrupt.
+* @param func The callback function.
+*/
+void (*leftptr)();
+void (*rightptr)();
+
+int leftspeed = 6;
+int rightspeed = 7;
+
+void callback(void (*ptr)()){ // callback function
+	ptr();
+}
+
+void EINT3_IRQHandler(void)
 {
-    const uint8_t last_pin_num = 63;
-    eint3_entry_t *e = NULL;
 
-    if (pin_num <= last_pin_num && NULL != func &&
-        NULL != (e = malloc(sizeof(*e)))
-       )
-    {
-        /* Insert new entry at the head of the list */
-        e->callback = func;
-        e->pin_mask = (UINT64_C(1) << pin_num);
-        e->next = m_eint3_list_head;
-        m_eint3_list_head = e;
+	if(LPC_GPIOINT->IntStatus & (1<<2))
+	{
+		LPC_GPIOINT->IO2IntClr = ( 0x3FFF << 0 );
+		if ( LPC_GPIO2 -> FIOPIN & ( 1 << leftspeed))
+			callback(leftptr);
+		else if ( LPC_GPIO2 -> FIOPIN & ( 1 << rightspeed))
+			callback(rightptr);
+	}
 
-        /* PORT0 */
-        if (pin_num < m_port2_offset) {
-            if (eint_falling_edge == type) LPC_GPIOINT->IO0IntEnF |= e->pin_mask;
-            else  LPC_GPIOINT->IO0IntEnR |= e->pin_mask;
-        }
-        /* PORT2 */
-        else {
-            if (eint_falling_edge == type) LPC_GPIOINT->IO2IntEnF |= (e->pin_mask >> m_port2_offset);
-            else LPC_GPIOINT->IO2IntEnR |= (e->pin_mask >> m_port2_offset);
-        }
-
-        /* EINT3 shares pin interrupts with Port0 and Port2 */
-        NVIC_EnableIRQ(EINT3_IRQn);
-    }
 }
 
 void eint3_enable_port0(uint8_t pin_num, eint_intr_t type, void_func_t func)
 {
-    eint3_enable((pin_num & 0x1F), type, func);
+	if ( pin_num == leftspeed)                                                    // used global variable to assign the value
+		leftspeed = func;
+	else if( pin_num == rightspeed)
+		rightspeed = func;
+
+	//ptr0 = func; // point function pointer to function passed in
+	//LPC_PINCON -> PINSEL0 &= ~( 1 << pin_num);
+	LPC_GPIO0 -> FIODIR0 &= ~(1 << pin_num);
+	if ( type == eint_rising_edge )
+	{
+		LPC_GPIOINT->IO0IntEnR |= ( 1 << pin_num);
+	}
+	if ( type == eint_falling_edge)
+	{
+		LPC_GPIOINT->IO0IntEnF |= ( 1 << pin_num);
+	}
+	NVIC_EnableIRQ(EINT3_IRQn);
 }
 
+/// @copydoc eint3_enable_port0()
 void eint3_enable_port2(uint8_t pin_num, eint_intr_t type, void_func_t func)
 {
-    eint3_enable((pin_num & 0x1F) + m_port2_offset, type, func);
+	if ( pin_num == leftspeed)                                                    // used global variable to assign the value
+		leftspeed = func;
+	else if( pin_num == rightspeed)
+		rightspeed = func;
+	LPC_GPIO2 -> FIODIR &= ~(1 << pin_num); // set pin as input
+
+	if ( type == eint_rising_edge )
+	{
+		LPC_GPIOINT->IO2IntEnR |= ( 1 << pin_num);
+	}
+	if ( type == eint_falling_edge)
+	{
+		LPC_GPIOINT->IO2IntEnF |= ( 1 << pin_num);
+	}
+	NVIC_EnableIRQ(EINT3_IRQn);
 }
