80a78b4c06ea5c9f8164efafc0c9aa7fbb663eed
Bento007 <trent.smith007@gmail.com>
2014-03-21 18:26:08 -0700

    shortened the folder paths

--- lpc1758_freertos_195/L5_Application/main.cpp
+++ lpc1758_freertos_195/L5_Application/main.cpp
@@ -25,7 +25,7 @@
  * @note  printf of %f may be turned off to save memory, this can be configured at sys_config.h
  */
 #include "tasks.hpp"
-//#include "examples/examples.hpp"
+#include "examples/examples.hpp"
 
 
 
@@ -46,80 +46,67 @@
 int main(void)
 {
 
-//    /**
-//     * A few basic tasks for this bare-bone system :
-//     *      1.  Terminal task provides gateway to interact with the board through UART terminal.
-//     *      2.  Remote task allows you to use remote control to interact with the board.
-//     *      3.  Wireless task responsible to receive, retry, and handle mesh network.
-//     *
-//     * Disable remote task if you are not using it.  Also, it needs ENABLE_TELEMETRY
-//     * such that it can save remote control codes to non-volatile memory.  IR remote
-//     * control codes can be learned by typing "learn" command.
-//     */
-//    scheduler_add_task(new terminalTask(PRIORITY_HIGH));
-//    scheduler_add_task(new remoteTask  (PRIORITY_LOW));
-//
-//    /* Consumes very little CPU, but need highest priority to handle mesh network ACKs */
-//    scheduler_add_task(new wirelessTask(PRIORITY_CRITICAL));
-//
-//    /* Your tasks should probably used PRIORITY_MEDIUM or PRIORITY_LOW because you
-//     * want the terminal task to always be responsive so you can poke around in
-//     * case something goes wrong.
-//     */
-//
-//    /**
-//     * This is a the board demonstration task that can be used to test the board.
-//     * This also shows you how to send a wireless packets to other boards.
-//     */
-//    #if 0
-//        scheduler_add_task(new example_io_demo());
-//    #endif
-//
-//    /**
-//     * Change "#if 0" to "#if 1" to enable examples.
-//     * Try these examples one at a time.
-//     */
-//    #if 0
-//        scheduler_add_task(new example_task());
-//        scheduler_add_task(new example_alarm());
-//        scheduler_add_task(new example_logger_qset());
-//        scheduler_add_task(new example_nv_vars());
-//    #endif
-//
-//    /**
-//	 * Try the rx / tx tasks together to see how they queue data to each other.
-//	 */
-//    #if 0
-//        scheduler_add_task(new queue_tx());
-//        scheduler_add_task(new queue_rx());
-//    #endif
-//
-//    /**
-//     * Another example of shared handles and producer/consumer using a queue.
-//     * In this example, producer will produce as fast as the consumer can consume.
-//     */
-//    #if 0
-//        scheduler_add_task(new producer());
-//        scheduler_add_task(new consumer());
-//    #endif
-//
-//    /**
-//     * If you have RN-XV on your board, you can connect to Wifi using this task.
-//     * This does two things for us:
-//     *   1.  The task allows us to perform HTTP web requests (@see wifiTask)
-//     *   2.  Terminal task can accept commands from TCP/IP through Wifly module.
-//     *
-//     * To add terminal command channel, add this at terminal.cpp :: taskEntry() function:
-//     * @code
-//     *     // Assuming Wifly is on Uart3
-//     *     addCommandChannel(Uart3::getInstance(), false);
-//     * @endcode
-//     */
-//    #if 0
-//        Uart3::getInstance().init(WIFI_BAUD_RATE, WIFI_RXQ_SIZE, WIFI_TXQ_SIZE);
-//        scheduler_add_task(new wifiTask(Uart3::getInstance(), PRIORITY_LOW));
-//    #endif
-//
-//    scheduler_start(true); ///< This shouldn't return
+    /**
+     * A few basic tasks for this bare-bone system :
+     *      1.  Terminal task provides gateway to interact with the board through UART terminal.
+     *
+     * Disable remote task if you are not using it.  Also, it needs ENABLE_TELEMETRY
+     * such that it can save remote control codes to non-volatile memory.  IR remote
+     * control codes can be learned by typing "learn" command.
+     */
+    scheduler_add_task(new terminalTask(PRIORITY_HIGH));
+
+    /* Your tasks should probably used PRIORITY_MEDIUM or PRIORITY_LOW because you
+     * want the terminal task to always be responsive so you can poke around in
+     * case something goes wrong.
+     */
+
+
+    /**
+     * Change "#if 0" to "#if 1" to enable examples.
+     * Try these examples one at a time.
+     */
+    #if 0
+        scheduler_add_task(new example_task());
+        scheduler_add_task(new example_alarm());
+        scheduler_add_task(new example_logger_qset());
+        scheduler_add_task(new example_nv_vars());
+    #endif
+
+    /**
+	 * Try the rx / tx tasks together to see how they queue data to each other.
+	 */
+    #if 0
+        scheduler_add_task(new queue_tx());
+        scheduler_add_task(new queue_rx());
+    #endif
+
+    /**
+     * Another example of shared handles and producer/consumer using a queue.
+     * In this example, producer will produce as fast as the consumer can consume.
+     */
+    #if 0
+        scheduler_add_task(new producer());
+        scheduler_add_task(new consumer());
+    #endif
+
+    /**
+     * If you have RN-XV on your board, you can connect to Wifi using this task.
+     * This does two things for us:
+     *   1.  The task allows us to perform HTTP web requests (@see wifiTask)
+     *   2.  Terminal task can accept commands from TCP/IP through Wifly module.
+     *
+     * To add terminal command channel, add this at terminal.cpp :: taskEntry() function:
+     * @code
+     *     // Assuming Wifly is on Uart3
+     *     addCommandChannel(Uart3::getInstance(), false);
+     * @endcode
+     */
+    #if 0
+        Uart3::getInstance().init(WIFI_BAUD_RATE, WIFI_RXQ_SIZE, WIFI_TXQ_SIZE);
+        scheduler_add_task(new wifiTask(Uart3::getInstance(), PRIORITY_LOW));
+    #endif
+
+    scheduler_start(true); ///< This shouldn't return
     return -1;
 }
