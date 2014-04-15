07be524a7bc3f94204e102cf94a08781c69ea74d
airybeartoe <evelasquez123@yahoo.com>
2014-04-15 16:15:31 -0700

    Dijkstra integration code added. Need to clean up code, finished the dijkstra task. Also have dijkstra transferring directions array to the state machine task.

--- lpc1758_freertos_195/L5_Application/main.cpp
+++ lpc1758_freertos_195/L5_Application/main.cpp
@@ -3,9 +3,14 @@
 #include "tasks.hpp"
 #include "examples/examples.hpp"
 #include "superwayTasks.hpp"
+#include "dijkstra.hpp"
 
 int main()
 {
+    //create the Queues in tasks...
+    SMtoPath = xQueueCreate(1, sizeof(path));
+    directionQ = xQueueCreate(20, sizeof(int));
+    //TODO: create other queues, if used.
 
     xTaskCreate(StateMachine, (const char*) "SMTask", STACK_BYTES(1024), 0, 0, 0);
     xTaskCreate(pathingTask, (const char*) "DjikstraTask", STACK_BYTES(4096), 0, 1, 0);
