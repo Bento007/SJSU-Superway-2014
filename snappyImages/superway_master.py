f5547327d283986700c4b32f551a0ea5792bc0f8
Bento007 <trent.smith007@gmail.com>
2014-04-18 13:06:57 -0700

    Edit: superway_master.py finished it's simple. it psudo registers vehicle and sends current time

--- snappyImages/superway_master.py
+++ snappyImages/superway_master.py
@@ -1,8 +1,8 @@
 """Provide GUI window display for MulticastCounter in Portal
 """
 import time
-import sqlite3
-conn = sqlite3.connect('SuperwayDB.db')
+# import sqlite3
+# conn = sqlite3.connect('SuperwayDB.db')
 
 
     
@@ -11,9 +11,9 @@ def getTimeRPC():
     rpc(remoteAddr,'getTime', xtime[0],xtime[1],xtime[2],xtime[3],xtime[4],xtime[5],xtime[6],xtime[7])
 def registerRPC(location):
     rpc(remoteAddr,'setActive')
-
+ 
 def setActive():
     pass
 def getTime():
     pass
-    
+     
