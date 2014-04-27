2fd0b9cc58cec13a5bf4d2801ab2aa4ed047723a
Trent Smith <Trent.smith007@gmail.com>
2014-04-27 11:18:36 -0700

    Update main.cpp

--- lpc1758_freertos_195/L5_Application/main.cpp
+++ lpc1758_freertos_195/L5_Application/main.cpp
@@ -1,20 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "tasks.hpp"
-#include "examples/examples.hpp"
 #include "superwayTasks.hpp"
 #include "dijkstra.hpp"
+#include "shared_handles.h"
 
 int main()
 {
     //create the Queues in tasks...
-    SMtoPath = xQueueCreate(1, sizeof(path));
+    SMtoPath = xQueueCreate(1, sizeof(path_t));
     directionQ = xQueueCreate(20, sizeof(int));
-    //TODO: create other queues, if used.
+    newDestinationQ = xQueueCreate(1, sizeof(uint32_t));
+    pathToSM = xQueueCreate(20, sizeof(SM_pkt));
+    lineFollowertoSM= xQueueCreate(1, sizeof(int));
+    WirelesstoSM = xQueueCreate(1, sizeof(int));
+    SMtoLineFollower = xQueueCreate(1, sizeof(int));
 
-    xTaskCreate(StateMachine, (const char*) "SMTask", STACK_BYTES(1024), 0, 0, 0);
-    xTaskCreate(pathingTask, (const char*) "DjikstraTask", STACK_BYTES(4096), 0, 1, 0);
-    xTaskCreate(wirelessTask, (const char*) "WirelessTask", STACK_BYTES(4096), 0, 2, 0);
+    //create the Semaphores in tasks....
+    ticks_sem = xSemaphoreCreateBinary();
+
+    //TODO: create other queues, if used.
+#if DEBUG
+    puts("In main, initializing Wireless Task");
+#endif
+
+
+    xTaskCreate(wirelessTask, (const char*) "WirelessTask", STACK_BYTES(2048), 0, PRIORITY_HIGH, 0);
+    xTaskCreate(StateMachine, (const char*) "SMTask", STACK_BYTES(2048), 0, PRIORITY_MEDIUM, 0);
+    xTaskCreate(pathingTask, (const char*) "DjikstraTask", STACK_BYTES(4096), 0, PRIORITY_CRITICAL, 0);
+    xTaskCreate(lineFollowerTask, (const char*) "LineFollowerTask", STACK_BYTES(2048), 0, PRIORITY_LOW, 0);
+//    xTaskCreate(updateTask, (const char*) "updateTask", STACK_BYTES(1024), 0, 0, 0);//
+    ////Pathing has been made a function instead. Phased out code remains as comments.
+
+
     vTaskStartScheduler();
     return -1;
 }
